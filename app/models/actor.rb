98e9f0ea380331d282dc1d690a8ed60002c8ffae
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-01 11:31:31 -0800

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        names = self.characters.map{|character| character.name}
+        show_names = self.characters.map{|character| character.show.name}
+        map = names.zip(show_names).map{|arr| "#{arr[0]} - #{arr[1]}"} 
+    end
 end
